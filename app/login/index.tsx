5dd681120577a557a5fa444a118b64b632f1719d
Heitor Rosa <heitorrs.dev@gmail.com>
2025-10-02 17:38:26 -0300

    feat: add basic login and register structure

---
+++ app/login/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import styles from './style';
+
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tela de Login</Text>
+    </View>
+  );
+}
---
+++ app/register/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+import styles from './style';
+export default function Login() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tela de Registro</Text>
+    </View>
+  );
+}
