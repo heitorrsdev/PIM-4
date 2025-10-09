b65bfd348570068e9940a3b1d664b737293f1bd0
Heitor Rosa <heitorrs.dev@gmail.com>
2025-10-09 17:59:30 -0300

    style: add global style files for shared use

---
+++ styles/colors.ts
@@ -0,0 +1,10 @@
+const colors = {
+  background: '#607D88',
+  error: '#DC2626',
+  primary: '#0A2E50',
+  secondary: '#1E3A8A',
+  textPrimary: '#111827',
+  textSecondary: '#4B5563',
+};
+
+export default colors;
