81c31104f22fe1db2f7c8a48035117e12cf8419e
oTK147 <takun147@outlook.com>
2025-10-24 17:20:27 -0300

    style: fix BaseHeader style

--- components/headers/BaseHeader/style.ts
+++ components/headers/BaseHeader/style.ts
@@ -10,10 +10,12 @@ const styles = StyleSheet.create({
     elevation: 6,
     paddingHorizontal: 16,
     paddingVertical: 8,
+    position: 'absolute',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 6 },
     shadowOpacity: 0.25,
     shadowRadius: 10,
+    width: '100%',
   },
   header: {
     alignItems: 'center',
