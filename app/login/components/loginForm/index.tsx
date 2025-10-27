e7194bc2e1b695ed9c524e92286e6dd37d96e50b
Heitor Rosa <heitorrs.dev@gmail.com>
2025-10-27 18:54:35 -0300

    feat: use useAuth hook in LoginForm

--- app/login/components/loginForm/index.tsx
+++ app/login/components/loginForm/index.tsx
@@ -1,31 +1,50 @@
-import { BaseForm, FormField } from '@/components/forms';
-import { isValidEmail } from '@/utils/validation';
 import { useState } from 'react';
 import { View } from 'react-native';
+
+import { BaseForm, FormField } from '@/components/forms';
+import { useAuth } from '@/hooks';
+import { isValidEmail } from '@/utils/validation';
+
 import styles from './styles';
 
+interface LoginErrors {
+  email?: string;
+  password?: string;
+}
+
 export default function LoginForm() {
+  const { login } = useAuth();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
+  const [errors, setErrors] = useState<LoginErrors>({});
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = () => {
-    const newErrors: { email?: string; password?: string } = {};
+  const handleSubmit = async () => {
+    const newErrors: LoginErrors = {};
+
     if (!email.trim()) newErrors.email = 'Email é obrigatório';
     else if (!isValidEmail(email)) newErrors.email = 'Email inválido';
+
     if (!password.trim()) newErrors.password = 'Senha é obrigatória';
 
     setErrors(newErrors);
 
-    if (Object.keys(newErrors).length === 0) {
-      // adicionar lógica de enviar dados posteriormente
-      console.log('Form enviado', { email, password });
+    if (Object.keys(newErrors).length !== 0) return;
+
+    try {
+      setLoading(true);
+      await login({ email, senha: password });
+      console.log('Login realizado com sucesso');
+    } catch (error: any) {
+      setErrors({ password: 'Email ou senha inválidos' });
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <View style={styles.container}>
-      <BaseForm onSubmit={handleSubmit} submitLabel="Registrar">
+      <BaseForm onSubmit={handleSubmit} submitLabel={loading ? 'Entrando...' : 'Entrar'}>
         <FormField
           label="Email"
           value={email}
