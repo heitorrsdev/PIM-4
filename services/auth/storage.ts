72137bd63147b0d23b0d85901e7c207146467b66
Heitor Rosa <heitorrs.dev@gmail.com>
2025-10-27 18:27:56 -0300

    feat: create storage.ts to manage JWT token in auth service folder

---
+++ services/auth/storage.ts
@@ -0,0 +1,28 @@
+import * as SecureStore from 'expo-secure-store';
+import { Platform } from 'react-native';
+
+const TOKEN_KEY = 'Suptech_token';
+
+export const saveToken = async (token: string) => {
+  if (Platform.OS === 'web') {
+    localStorage.setItem(TOKEN_KEY, token);
+  } else {
+    await SecureStore.setItemAsync(TOKEN_KEY, token);
+  }
+};
+
+export const getToken = async (): Promise<string | null> => {
+  if (Platform.OS === 'web') {
+    return localStorage.getItem(TOKEN_KEY);
+  } else {
+    return await SecureStore.getItemAsync(TOKEN_KEY);
+  }
+};
+
+export const removeToken = async () => {
+  if (Platform.OS === 'web') {
+    localStorage.removeItem(TOKEN_KEY);
+  } else {
+    await SecureStore.deleteItemAsync(TOKEN_KEY);
+  }
+};
