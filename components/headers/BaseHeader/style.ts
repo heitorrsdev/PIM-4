5c1f1db1ac30e114006d55f8314fb1ba5499beb4
oTK147 <takun147@outlook.com>
2025-10-28 17:25:16 -0300

    style: fix BaseHeader position

--- components/headers/BaseHeader/style.ts
+++ components/headers/BaseHeader/style.ts
@@ -3,15 +3,17 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'rgba(20, 30, 48, 0.9)',
-    paddingHorizontal: 16,
-    paddingVertical: 8,
     borderBottomLeftRadius: 14,
     borderBottomRightRadius: 14,
+    elevation: 6,
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    position: 'absolute',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 6 },
     shadowOpacity: 0.25,
     shadowRadius: 10,
-    elevation: 6,
+    width: '100%',
   },
   header: {
     flexDirection: 'row',
