d53fc6f3a92c0c718bfdad9c68210245ee5440c2
Heitor Rosa <heitorrs.dev@gmail.com>
2025-11-02 18:38:21 -0300

    refactor: remove elses

--- services/auth/storage.ts
+++ services/auth/storage.ts
@@ -6,23 +6,22 @@ const TOKEN_KEY = 'Suptech_token';
 export const saveToken = async (token: string): Promise<void> => {
   if (Platform.OS === 'web') {
     localStorage.setItem(TOKEN_KEY, token);
-  } else {
-    await SecureStore.setItemAsync(TOKEN_KEY, token);
+    return;
   }
+  await SecureStore.setItemAsync(TOKEN_KEY, token);
 };
 
 export const getToken = async (): Promise<string | null> => {
   if (Platform.OS === 'web') {
     return localStorage.getItem(TOKEN_KEY);
-  } else {
-    return await SecureStore.getItemAsync(TOKEN_KEY);
   }
+  return await SecureStore.getItemAsync(TOKEN_KEY);
 };
 
 export const removeToken = async (): Promise<void> => {
   if (Platform.OS === 'web') {
     localStorage.removeItem(TOKEN_KEY);
-  } else {
-    await SecureStore.deleteItemAsync(TOKEN_KEY);
+    return;
   }
+  await SecureStore.deleteItemAsync(TOKEN_KEY);
 };
