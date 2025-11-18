54715f804ef341da50cafd1fa834afbeeaa26b4b
Heitor Rosa <heitorrs.dev@gmail.com>
2025-11-18 17:37:43 -0300

    fix: set loadingUser to false only after successful user validation

--- contexts/UserContext.tsx
+++ contexts/UserContext.tsx
@@ -33,7 +33,6 @@ export const UserProvider = ({ children }: UserProviderProps) => {
 
     if (!isAuthenticated || !token || !email) {
       setUser(null);
-      setLoadingUser(false);
       return;
     }
 
